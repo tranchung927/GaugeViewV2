92ffe5650d6f0ff10ddde1f08dac4edb4ea885c3
ChungTran <chungtv@tadi.io>
2017-10-13 17:34:32 +0700

    change value

--- GaugeViewV2/ViewController.swift
+++ GaugeViewV2/ViewController.swift
@@ -9,28 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var gaugeView: GaugeView!
     
     var value = 0
-    var valueDisplay = 0
+    var isReturn: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
         
         gaugeView.percentage = Float(value)
-        gaugeView.labelText = "\(valueDisplay)%"
+        gaugeView.labelText = "\(value)%"
     }
     @IBAction func update(_ sender: Any) {
-        if valueDisplay < 100 {
-            valueDisplay += 10
+        isReturn = !isReturn
+        if isReturn {
+            for i in 0...100 {
+                value = i
+                gaugeView.percentage = Float(value)
+                gaugeView.labelText = "\(value)%"
+            }
+        } else {
+            value = 0
+            gaugeView.percentage = Float(value)
+            gaugeView.labelText = "\(value)%"
         }
-        else if valueDisplay == 100 {
-            valueDisplay = 10
-        }
-        value += 10
-        gaugeView.percentage = Float(value)
-        gaugeView.labelText = "\(valueDisplay)%"
-        print(value)
+        //        if valueDisplay < 100 {
+        //            valueDisplay += 10
+        //        }
+        //        else if valueDisplay == 100 {
+        //            valueDisplay = 10
+        //        }
+        //        value += 10
+        //        gaugeView.percentage = Float(value)
+        //        gaugeView.labelText = "\(valueDisplay)%"
+        //        print(value)
     }
 }
 
